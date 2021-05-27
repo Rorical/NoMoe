76f6266d49f4bbe5f77f56d5e5cc5d22dfda353e
Rorical <hash_548@163.com>
2021-05-27 16:37:03 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    Welcome to Your Vue.js App"
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
